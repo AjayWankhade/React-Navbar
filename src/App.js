8b5f7499602dc9128acc0596ee0279e8067f4b50
Ajay <ajaywankhade666@gmail.com>
2023-12-28 21:23:59 +0530

    navbar from react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from '../src/components/Home';
+import Contact from '../src/components/Contact';
+import About from '../src/components/About';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return( 
+    <BrowserRouter>
+  <div>
+   
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact" >Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+    
+      <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+
+      </Routes>
+      </div>
+      
+ 
+
+  </div>
+  </BrowserRouter>
+  )
 }
 
 export default App;
